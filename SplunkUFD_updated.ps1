b85fbadfca923d74069fc05e77c4b2150f168f6e
Gotarr <47251800+Gotarr@users.noreply.github.com>
2024-09-09 11:00:09 +0200

    shorter

--- SplunkUFD_updated.ps1
+++ SplunkUFD_updated.ps1
@@ -1,4 +1,3 @@
-$UFInstalled = "C:\Program Files\SplunkUniversalForwarder\etc\splunk.version"
 $ScriptPfad = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $SplunkInstaller = "splunkforwarder-9.2.1-78803f08aabb-x64-release.msi"
 $FQDN = (Get-CimInstance -ClassName Win32_ComputerSystem).DNSHostName + "." + (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
@@ -11,19 +10,11 @@ if (-not (Test-Path -Path $UFInstalled)) {
         exit 1
     }
 } else {
-    $UFVersion = (Get-Content $UFInstalled | Select-String -Pattern "VERSION").Line
-    $InstallerVersion = "VERSION=9.2.1"  # Die Version sollte hier manuell aktualisiert werden
-
-    if ($UFVersion -eq $InstallerVersion) {
-        Write-Host "Kein Versionsunterschied, aktuelle Version vorhanden, breche Upgrade ab!"
-        exit 0
-    } else {
-        Write-Host "Unterschied erkannt, Beginne Upgrade"
-        try {
-            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\$SplunkInstaller AGREETOLICENSE=yes /quiet" -Wait
-        } catch {
-            Write-Error "Fehler beim Upgrade des Splunk Universal Forwarders: $_"
-            exit 1
-        }
+    Write-Host "Splunk Universal Forwarder ist bereits installiert. Beginne Upgrade"
+    try {
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\$SplunkInstaller AGREETOLICENSE=yes /quiet" -Wait
+    } catch {
+        Write-Error "Fehler beim Upgrade des Splunk Universal Forwarders: $_"
+        exit 1
     }
 }
