8dcee62fb1a95148cd1ed60f15937957346e2b31
Gotarr <47251800+Gotarr@users.noreply.github.com>
2024-09-09 10:58:31 +0200

    set msi path and remove version

--- SplunkUFD_updated.ps1
+++ SplunkUFD_updated.ps1
@@ -1,33 +1,26 @@
 $UFInstalled = "C:\Program Files\SplunkUniversalForwarder\etc\splunk.version"
 $ScriptPfad = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$UFUpgrade = Join-Path -Path $ScriptPfad -ChildPath "splunk.version"
+$SplunkInstaller = "splunkforwarder-9.2.1-78803f08aabb-x64-release.msi"
 $FQDN = (Get-CimInstance -ClassName Win32_ComputerSystem).DNSHostName + "." + (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
 
 if (-not (Test-Path -Path $UFInstalled)) {
     try {
-        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\splunkforwarder-9.2.1-78803f08aabb-x64-release.msi AGREETOLICENSE=yes /quiet" -Wait
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\$SplunkInstaller AGREETOLICENSE=yes /quiet" -Wait
     } catch {
         Write-Error "Fehler bei der Installation des Splunk Universal Forwarders: $_"
         exit 1
     }
 } else {
-    $UnterschiedPlattform = Compare-Object -ReferenceObject (Get-Content $UFInstalled) -DifferenceObject (Get-Content $UFUpgrade) | Where-Object { $_.InputObject -Like "PLATFORM*" }
-    $UnterschiedVersion = Compare-Object -ReferenceObject (Get-Content $UFInstalled) -DifferenceObject (Get-Content $UFUpgrade) | Where-Object { $_.InputObject -Like "VERSION*" }
+    $UFVersion = (Get-Content $UFInstalled | Select-String -Pattern "VERSION").Line
+    $InstallerVersion = "VERSION=9.2.1"  # Die Version sollte hier manuell aktualisiert werden
 
-    if (-not $UnterschiedPlattform) {
-        Write-Host "Richtige Plattform, Weiter mit Versioncheck"
-    } else {
-        Write-Host "Abbruch Falsches Paket ausgewählt (LINUX Forwarder?)"
-        exit 1
-    }
-
-    if (-not $UnterschiedVersion) {
+    if ($UFVersion -eq $InstallerVersion) {
         Write-Host "Kein Versionsunterschied, aktuelle Version vorhanden, breche Upgrade ab!"
         exit 0
     } else {
-        Write-Host "Unterschied erkannt und Plattformcheck ist ok, Beginne Upgrade"
+        Write-Host "Unterschied erkannt, Beginne Upgrade"
         try {
-            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\splunkforwarder-9.2.1-78803f08aabb-x64-release.msi AGREETOLICENSE=yes /quiet" -Wait
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $ScriptPfad\$SplunkInstaller AGREETOLICENSE=yes /quiet" -Wait
         } catch {
             Write-Error "Fehler beim Upgrade des Splunk Universal Forwarders: $_"
             exit 1
